f109f24a2d64b86e132d9d8f5c4b2f65eaa102ad
RossWilliamson1 <39889940+RossWilliamson1@users.noreply.github.com>
2019-10-14 23:16:36 +0100

    Finished off the first part of the assignment
    Client now sends the number 220 to the server and displays what it gets back

--- Assignment 1/src/Client.java
+++ Assignment 1/src/Client.java
@@ -13,22 +13,25 @@ public class Client {
         socket = new Socket("127.0.0.1", 5000);
     }
 
-    public void SendObject(Object obj) throws IOException{
+    public void SendObject(Object first, Object second) throws IOException{
         ObjectOutputStream oStream = new ObjectOutputStream(socket.getOutputStream());
-        oStream.writeObject(obj);
+        oStream.writeObject(first);
+        oStream.writeObject(second);
     }
 
     public void ClientReceive() throws IOException, ClassNotFoundException {
         ObjectInputStream streamI = new ObjectInputStream(socket.getInputStream());
-        String message = (String) streamI.readObject();
-        System.out.println("Message Received: " + message);
+        String messageOne = (String) streamI.readObject();
+        Integer messageTwo = (Integer) streamI.readObject();
+        System.out.println("Message Received: " + messageOne);
+        System.out.println("Message Received: " + messageTwo);
     }
 
 
     public static void main(String[] args){
         try {
             Client client = new Client();
-            client.SendObject("text");
+            client.SendObject("text", "220");
             client.ClientReceive();
         }catch(Exception e){e.printStackTrace();}
     }
