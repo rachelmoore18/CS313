765aeddfb1731cca4b2df9452b0478a6ac1d3895
ayelagilani <44972353+ayelagilani@users.noreply.github.com>
2019-10-21 16:15:38 +0100

    Update Client.java

--- Assignment 1/src/Client.java
+++ Assignment 1/src/Client.java
@@ -13,25 +13,22 @@ public class Client {
         socket = new Socket("127.0.0.1", 5000);
     }
 
-    public void SendObject(Object first, Object second) throws IOException{
+    public void SendObject(Object obj) throws IOException{
         ObjectOutputStream oStream = new ObjectOutputStream(socket.getOutputStream());
-        oStream.writeObject(first);
-        oStream.writeObject(second);
+        oStream.writeObject(obj);
     }
 
     public void ClientReceive() throws IOException, ClassNotFoundException {
         ObjectInputStream streamI = new ObjectInputStream(socket.getInputStream());
-        String messageOne = (String) streamI.readObject();
-        Integer messageTwo = (Integer) streamI.readObject();
-        System.out.println("Message Received: " + messageOne);
-        System.out.println("Message Received: " + messageTwo);
+        String message = (String) streamI.readObject();
+        System.out.println("Message Received: " + message);
     }
 
 
     public static void main(String[] args){
         try {
             Client client = new Client();
-            client.SendObject("text", "220");
+            client.SendObject("text");
             client.ClientReceive();
         }catch(Exception e){e.printStackTrace();}
     }
